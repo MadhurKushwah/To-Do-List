6ffc0ce2b8ac008c32f6e56b666117f08954ac0a
Madhur <81923994+MadhurKushwah@users.noreply.github.com>
2024-02-21 15:00:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+
+function addTask() {
+    var taskInput = document.getElementById('taskInput');
+    var taskList = document.getElementById('taskList');
+
+    if (taskInput.value.trim() !== '') {
+        var li = document.createElement('li');
+        li.textContent = taskInput.value;
+        taskList.appendChild(li);
+        taskInput.value = '';
+    } else {
+        alert('Please enter a task!');
+    }
+}
